160d4ef97f3f229ecb40a6792f6b720aad5d21d8
rajpatelrp2503-dev <rajpatelrp2503@gmail.com>
2026-01-15 17:52:32 +0530

    Change auth setting

--- Backend/app/authentication/auth.py
+++ Backend/app/authentication/auth.py
@@ -8,8 +8,8 @@ import os
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
